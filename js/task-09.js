6fae4a6dd4536788236c90ca1dee82a8b6a6c79a
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-05-06 00:56:37 +0300

    all task done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+function onChangeColorBtn() {
+  document.body.style.background = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
+
+changeColorBtn.addEventListener('click', onChangeColorBtn);
