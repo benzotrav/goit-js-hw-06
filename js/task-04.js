8ba155d1073c94e32d315407a38a829767f2e159
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-05-05 23:19:11 +0300

    task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+let counterValue = 0;
+const value = document.querySelector("#value");
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+decrementBtn.addEventListener('click', () => {
+    counterValue -= 1;
+    value.textContent = counterValue;
+});
+
+incrementBtn.addEventListener('click', () => {
+    counterValue += 1;
+    value.textContent = counterValue;
+});
